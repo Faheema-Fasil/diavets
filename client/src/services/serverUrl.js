27e9c9b57bbc39e842a8c0e87138f619faee9879
Faheema Fasil <faheemafasil12@gmail.com>
2025-06-10 15:38:32 +0530

    changes

--- client/src/services/serverUrl.js
+++ client/src/services/serverUrl.js
@@ -1,4 +1,5 @@
 // const server_url = "";
-const server_url = "http://localhost:5000";
+// const server_url = "http://localhost:5000";
+const server_url = "https://diavets.onrender.com";
 // const server_url2 = "http://localhost:5000";
 export default server_url;
